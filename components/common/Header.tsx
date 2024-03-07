224144386604fac6fef1c44a00dfe16ea26f3ee2
anubhav-aryan <aryananubhav08@gmail.com>
2024-03-07 22:55:20 +0530

    feat: header fix

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
-  )
-}
+    <>
+      <div className="text-gray-600 body-font">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <Link href="/">
+            <Image src="/VIT Logo.svg" alt="logo" width={200} height={100} />
+          </Link>
+          <div className="md:ml-auto flex flex-wrap items-center text-lg justify-center text-black gap-24">
+            <Link className=" " href="/committees">COMMITTEES</Link>
+            <Link className=" " href="/about">ABOUT US</Link>
+            <Link className=" " href="resources">RESOURCES</Link>
+            <Link className=" " href="faqs">FAQs</Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Header
+export default Header;
