6ba61cddac4365fd5b0947748711a4b602eed4f2
woaitsAryan <yoprince.ab@gmail.com>
2024-03-10 22:42:54 +0530

    fix: cleaned up google analytics script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,11 +17,8 @@ export default function App({ Component, pageProps }: AppProps) {
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'G-RLBBSTEDWR',{
-          page_path: window.location.pathname,
-          
-});
-         `}
+          gtag('config', 'G-RLBBSTEDWR');
+      `}
     </Script>
 
     <ToastContainer
