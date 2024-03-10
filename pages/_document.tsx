c0812c795fe5b60b0040dee682630ebbbef1e320
Keshav-Aneja <keshav.aneja09@gmail.com>
2024-03-10 13:04:52 +0530

    feat: favicon added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>VITMUN'24</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
