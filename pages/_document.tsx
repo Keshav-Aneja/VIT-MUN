417cf79f473f9acbbba20b97e45a312ee638be0e
woaitsAryan <yoprince.ab@gmail.com>
2024-03-10 12:06:59 +0530

    feat: Added google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,10 @@
+import { GoogleAnalytics } from "@next/third-parties/google";
 import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <GoogleAnalytics gaId="G-RLBBSTEDWR"/>
       <body>
         <Main />
         <NextScript />
