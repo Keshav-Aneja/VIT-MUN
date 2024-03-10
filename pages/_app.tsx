96146b6fa4c6b459bd7f0c6a211429b920675750
woaitsAryan <yoprince.ab@gmail.com>
2024-03-10 12:23:34 +0530

    feat: another google analytics add

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,31 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import 'react-toastify/dist/ReactToastify.css'
 import { ToastContainer } from 'react-toastify'
-
+import Script from 'next/script'
 export default function App({ Component, pageProps }: AppProps) {
   return <>
     <Component {...pageProps} />
-    <ToastContainer 
-    
+
+    <Script
+      strategy="lazyOnload"
+      src={`https://www.googletagmanager.com/gtag/js?id=G-RLBBSTEDWR`}
+    />
+
+    <Script strategy="lazyOnload">
+      {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-RLBBSTEDWR',{
+          page_path: window.location.pathname,
+          
+![image](https://cdn.sanity.io/images/dgsq0x0m/production/61f24596fb210134e5df6a1dfa285de0f24791cc-1549x601.png?w=450)});
+         `}
+    </Script>
+
+    <ToastContainer
+
     ></ToastContainer>
-  
+
   </>;
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,8 @@
-import { GoogleAnalytics } from "@next/third-parties/google";
 import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <GoogleAnalytics gaId="G-RLBBSTEDWR"/>
       <body>
         <Main />
         <NextScript />
